60c952710f4aff0f0525e6f349337d4f22c85fd4
Edward Tischler <etischler@manh.com>
2018-05-23 19:55:39 -0400

    Math|ExcelColumnTitle

--- src/Math/ExcelColumnTitle.java
+++ src/Math/ExcelColumnTitle.java
@@ -1,10 +1,28 @@
-public class Template {
+package Math;
+
+public class ExcelColumnTitle {
 
     public static void main(String[] args) {
-
+        System.out.println(convertToTitle(257));
+        System.out.println(convertToTitle(676));
+        System.out.println(convertToTitle(1000));
     }
 
-    static void solution() {
+    public static String convertToTitle(int A) {
+        String ans = "";
+        while(A > 0) {
+           int remainder = A % 26;
 
+           if(remainder == 0) {
+               ans += 'Z';
+               A = (A / 26) - 1;
+           }
+           else{
+               ans += (char) ('@' + remainder);
+               A /= 26;
+           }
+        }
+
+        return new StringBuilder(ans).reverse().toString();
     }
 }
