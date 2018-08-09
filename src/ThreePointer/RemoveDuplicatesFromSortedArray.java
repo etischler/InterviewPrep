31e20ad207ca13bddcf18e7aa15f1aa9f2ebaccd
Edward Tischler <etischler@manh.com>
2018-08-09 18:19:39 -0400

    ThreePointer|RemoveDuplicatesFromSortedArray

---
+++ src/ThreePointer/RemoveDuplicatesFromSortedArray.java
@@ -0,0 +1,31 @@
+package ThreePointer;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
+public class RemoveDuplicatesFromSortedArray {
+
+    public static void main(String[] args) {
+        //0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3
+        System.out.println(removeDuplicates(new ArrayList<>(Arrays.asList(1,1,2))));
+        System.out.println(removeDuplicates(new ArrayList<>(Arrays.asList(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3))));
+    }
+
+    static int removeDuplicates(ArrayList<Integer> a) {
+        if(a.size() == 0)
+            return 0;
+        int size = 1;
+        int lastVal = a.get(0);
+        for(int i = 1; i < a.size(); i++) {
+            if(lastVal != a.get(i)) {
+                lastVal = a.get(i);
+                size++;
+                a.set(size - 1, a.get(i));
+            }
+
+        }
+        System.out.println(a);
+        return size;
+    }
+}
